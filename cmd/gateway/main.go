82f5ccdb18f8716d2a2006cb23c349547910c8a6
djiwandou-p <djiwandou@gmail.com>
2023-08-16 07:40:12 +0700

    init sse and netlify setup

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,35 +1,66 @@
 package main
 
 import (
-	"flag"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
-	"github.com/carlmjohnson/feed2json"
-	"github.com/carlmjohnson/gateway"
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
-	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
-	flag.Parse()
-	listener := gateway.ListenAndServe
-	portStr := ""
-	if *port != -1 {
-		portStr = fmt.Sprintf(":%d", *port)
-		listener = http.ListenAndServe
-		http.Handle("/", http.FileServer(http.Dir("./public")))
-	}
-
-	http.Handle("/api/feed", feed2json.Handler(
-		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
-		nil, nil, nil, cacheControlMiddleware))
-	log.Fatal(listener(portStr, nil))
+type Client struct {
+	name   string
+	events chan *DashBoard
+}
+type DashBoard struct {
+	User uint
 }
 
-func cacheControlMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "public, max-age=300")
-		h.ServeHTTP(w, r)
-	})
+func main() {
+	app := fiber.New()
+	app.Get("/sse", adaptor.HTTPHandler(handler(dashboardHandler)))
+	app.Listen(":9001")
+}
+
+func handler(f http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(f)
+}
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	client := &Client{name: r.RemoteAddr, events: make(chan *DashBoard, 10)}
+	go updateDashboard(client)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	timeout := time.After(1 * time.Second)
+	select {
+	case ev := <-client.events:
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		enc.Encode(ev)
+		fmt.Fprintf(w, "data: %v\n\n", buf.String())
+		fmt.Printf("data: %v\n", buf.String())
+	case <-timeout:
+		fmt.Fprintf(w, ": nothing to sent\n\n")
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
+}
+
+func updateDashboard(client *Client) {
+	for {
+		db := &DashBoard{
+			User: uint(rand.Uint32()),
+		}
+		client.events <- db
+	}
 }
